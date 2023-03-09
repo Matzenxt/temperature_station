de0c5a212369892d6682cde1d0208087d1563793
Matthias Lodner <matthias@lodner.de>
2023-03-09 21:43:26 +0100

    Removed functionality for server.

--- server/src/main.rs
+++ server/src/main.rs
@@ -6,7 +6,6 @@ use actix_web::{App, http, HttpServer, middleware};
 use actix_web::web::Data;
 use sqlx::sqlite::SqlitePoolOptions;
 use dotenv::dotenv;
-use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 use crate::service::measurement::{add_measurement, get_all_measurements, get_by_search};
 use crate::service::room::get_all_room;
 
@@ -23,19 +22,9 @@ async fn main() -> io::Result<()> {
     let server_address = env::var("SERVER_ADDRESS").expect("SERVER_ADDRESS is not set in .env file");
     let server_port = env::var("SERVER_PORT").expect("SERVER_PORT is not set in .env file");
     let database = env::var("DATABASE_URL").expect("DATABASE is not set in .env file");
-    let dir_private_key = env::var("DIR_PRIVATE_KEY").expect("DIR_PRIVATE_KEY is not set in .env file");
-    let dir_certificate_chain = env::var("DIR_CERTIFICATE_CHAIN").expect("DIR_CERTIFICATE_CHAIN is not set in .env file");
 
     env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
     env_logger::init();
-
-    /*
-    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-    builder
-        .set_private_key_file(dir_private_key, SslFiletype::PEM)
-        .unwrap();
-    builder.set_certificate_chain_file(dir_certificate_chain).unwrap();
-*/
 
     let pool = SqlitePoolOptions::new()
         .max_connections(60)
@@ -47,12 +36,7 @@ async fn main() -> io::Result<()> {
             .allowed_methods(vec!["GET", "POST"])
             .allowed_headers(vec![
                 http::header::ACCESS_CONTROL_ALLOW_ORIGIN,
-                http::header::AUTHORIZATION,
-                http::header::ACCEPT,
-                http::header::CONTENT_TYPE,
-                http::header::ACCESS_CONTROL_ALLOW_CREDENTIALS,
             ])
-            .supports_credentials()
             .max_age(3600);
 
         App::new()
@@ -68,7 +52,6 @@ async fn main() -> io::Result<()> {
         .service(get_all_room)
     })
         .bind(format!("{}:{}", server_address, server_port))?
-        //.bind_openssl(format!("{}:{}", server_address, server_port), builder)?
         .run()
         .await
 }
